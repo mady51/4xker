a076366a38e39f70e6f0eb1f47208ce8515759e6
Karthikeyan Mani <kmani@codeaurora.org>
2017-04-12 15:42:18 -0700

    ASoC: wcd9335: Initialize variables before use
    Initialize variable before use to avoid
    invalid memory access.
    
    CRs-fixed: 2004067
    Change-Id: I719bb13acb1ae8a9654604d251f728d98cffe936
    Signed-off-by: Karthikeyan Mani <kmani@codeaurora.org>

--- sound/soc/codecs/wcd9335.c
+++ sound/soc/codecs/wcd9335.c
@@ -5018,7 +5018,11 @@ static int tasha_codec_enable_spline_src(struct snd_soc_codec *codec,
 					 int src_num,
 					 int event)
 {
+	u16 rx_path_cfg_reg;
+	u16 rx_path_ctl_reg;
+	u16 src_clk_reg;
 	u16 src_paired_reg = 0;
+	int *src_users, count, spl_src;
 	struct tasha_priv *tasha;
 	u16 rx_path_cfg_reg = WCD9335_CDC_RX1_RX_PATH_CFG0;
 	u16 rx_path_ctl_reg = WCD9335_CDC_RX1_RX_PATH_CTL;
